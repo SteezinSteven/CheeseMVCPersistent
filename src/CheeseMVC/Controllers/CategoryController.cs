d43c093aa620d15110285c0e9f5552a68a56d054
Steven Rausch <stevenrausch44@gmail.com>
2018-12-20 14:21:33 -0600

    part 2 finished

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: Category
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+            return View(categories);
+
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel catViewModel = new AddCategoryViewModel();
+            return View(catViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel catviewmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory { Name = catviewmodel.Name
+                };
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("/Category");
+            }
+            
+            return View(catviewmodel);
+        }
+
+    }  
+}
--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -17,32 +17,32 @@ namespace CheeseMVC.ViewModels
         [Required(ErrorMessage = "You must give your cheese a description")]
         public string Description { get; set; }
 
-        public CheeseType Type { get; set; }
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+        public List<SelectListItem> Categories { get; set; }
 
-        public List<SelectListItem> CheeseTypes { get; set; }
 
-        public AddCheeseViewModel() {
 
-            CheeseTypes = new List<SelectListItem>();
+        //public List<SelectListItem> CheeseTypes { get; set; }
 
-            // <option value="0">Hard</option>
-            CheeseTypes.Add(new SelectListItem {
-                Value = ((int) CheeseType.Hard).ToString(),
-                Text = CheeseType.Hard.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
+        public AddCheeseViewModel() { }
+        public AddCheeseViewModel(IEnumerable<CheeseCategory> categories) { 
+        
+            Categories = new List<SelectListItem>();
+            foreach (var cat in categories)
+            Categories.Add(new SelectListItem
             {
-                Value = ((int)CheeseType.Soft).ToString(),
-                Text = CheeseType.Soft.ToString()
+                Value = ((int)cat.ID).ToString(),
+                Text = cat.Name.ToString()
             });
-
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Fake).ToString(),
-                Text = CheeseType.Fake.ToString()
-            });
-
         }
+
+       
+            
+
+
+
+        
     }
 }
