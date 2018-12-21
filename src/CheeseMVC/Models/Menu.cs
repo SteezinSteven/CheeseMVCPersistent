397462e084b1838dff4ec5820e092735048f2085
Steven Rausch <stevenrausch44@gmail.com>
2018-12-20 20:17:51 -0600

    Complete

---
+++ src/CheeseMVC/Models/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class Menu
+    {
+        IList<CheeseMenu> CheeseMenus = new List<CheeseMenu>();
+
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+
+
+}
